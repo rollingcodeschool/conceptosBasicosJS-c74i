da7bde16ec4b9062e0084706a6a67f36ccd8dd91
Emilse Arias <emilsearias@gmail.com>
2023-10-17 11:51:51 -0300

    agregamos mensajes por consola

--- js/1-variables.js
+++ js/1-variables.js
@@ -8,5 +8,9 @@ multiples lineas
 
 //inicializar una variables (var, let, const)
 
-let comision = "c74i"; 
+let comision = "c74i";
 
+//mostrar mensajes
+console.log(7+8);
+console.log(comision);
+console.log( "comision: " + comision);
