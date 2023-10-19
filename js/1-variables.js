f8e1667816a233c556d279f338eebea142b99ffc
Emilse Arias <emilsearias@gmail.com>
2023-10-19 09:53:17 -0300

    agregue la suma de alejo

--- js/1-variables.js
+++ js/1-variables.js
@@ -14,3 +14,12 @@ let comision = "c74i";
 console.log(7+8);
 console.log(comision);
 console.log( "comision: " + comision);
+
+
+let numero1 = 10;
+let numero2 = 11;
+// let resultado = null;
+// Suma de numero1 y numero2.
+// resultado = numero1 + numero2;
+console.log("El resultado de la suma es: "+ (numero1 + numero2));
+
