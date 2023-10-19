7ac3fbab97387cd035ed3d6e878f269df6f5eb1e
Emilse Arias <emilsearias@gmail.com>
2023-10-19 10:28:25 -0300

    agregamos un prompt para pedir un color a un usuario

--- js/1-variables.js
+++ js/1-variables.js
@@ -23,3 +23,18 @@ let numero2 = 11;
 // resultado = numero1 + numero2;
 console.log("El resultado de la suma es: "+ (numero1 + numero2));
 
+// muestra el mensaje en el documento html
+document.write("<h1 class='tituloPrincipal'>Hello world <br> soy batman</h1>");
+
+// ventanas emergentes
+// alert("esto es una alerta");
+
+// solicitar un color al usuario
+let color = prompt("Ingrese su color favorito");
+
+console.log(color);
+//modificamos el contenido de la variable
+color = "Rojo";
+console.log(color);
+
+
