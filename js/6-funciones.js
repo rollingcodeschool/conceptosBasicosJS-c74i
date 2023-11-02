47ef00b893a3c27db0430a99732f34663fb5b1cd
Emilse Arias <emilsearias@gmail.com>
2023-11-02 11:14:45 -0300

    agregamos una funcion en forma de flecha

--- js/6-funciones.js
+++ js/6-funciones.js
@@ -8,6 +8,20 @@ function nuevoSaludo(nombreIngresado, apellidoIngresado){
     // document.write("<p>Hola "+nombreIngresado+ ", "+apellidoIngresado+" que tengas un buen día</p>");
 }
 
+// function conversionAPesos(dolares){
+//     const precioFinal = dolares * 1000;
+//     console.log(precioFinal);
+//     return precioFinal;
+// }
+//arrow functions
+// const conversionAPesos = (dolares) =>{
+//     const precioFinal = dolares * 1000;
+//     console.log(precioFinal);
+//     return precioFinal;
+// }
+
+const conversionAPesos = (dolares) => dolares * 1000;
+
 //invocar a una funcion
 saludar();
 
@@ -21,3 +35,10 @@ nuevoSaludo('Luciano', 'Cabello');
 nuevoSaludo('Sebastian');
 
 nuevoSaludo();
+
+let precioProducto = conversionAPesos(35);
+document.write(`<p>El precio del producto de prueba es: $${precioProducto}</p>`);
+document.write(`<p>El precio del producto de prueba es: $${conversionAPesos(40)}</p>`);
+
+
+
