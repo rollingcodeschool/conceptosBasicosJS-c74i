778a0e4a88f6e834d3802242b0349b177a026eb0
Emilse Arias <emilsearias@gmail.com>
2023-11-02 11:34:19 -0300

    reemplazamos el codigo duplicado del archivo de arrays por una funcion que muestre la lista de peliculas

--- js/5-arrays.js
+++ js/5-arrays.js
@@ -1,7 +1,23 @@
 //declaremos un array vacio
 let colores = [];
 
-const peliculas = ['dr strange','rambo','La saga de Transformer','Five nights at freedy', true, 2023 ];
+const peliculas = [
+  "dr strange",
+  "rambo",
+  "La saga de Transformer",
+  "Five nights at freedy",
+  true,
+  2023,
+];
+
+const mostrarPeliculas = (titulo) => {
+  document.write(`<h2>Lista de peliculas ${titulo}</h2>`);
+  document.write(`<ul>`);
+  for (let i = 0; i < peliculas.length; i++) {
+    document.write(`<li>${peliculas[i]}</li>`);
+  }
+  document.write(`</ul>`);
+};
 
 //cantidad de elementos en un array
 console.log(peliculas.length);
@@ -10,74 +26,37 @@ console.log(peliculas.length - 1);
 //mostrar por pantalla un array
 document.write(peliculas);
 document.write(`<p>El primer elemento del array es: ${peliculas[0]}</p>`);
-document.write(`<p>El ultimo elemento del array es: ${peliculas[peliculas.length-1]}</p>`);
+document.write(
+  `<p>El ultimo elemento del array es: ${peliculas[peliculas.length - 1]}</p>`
+);
 document.write(`<p>Un elemento inexistente del array es: ${peliculas[10]}</p>`);
 
-document.write(`<h2>Lista de peliculas</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas('');
 
 //agregar elementos en el array
-peliculas.unshift('Ironman 1','Ironman 2');
-document.write(`<h2>Lista de peliculas + ironman</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+peliculas.unshift("Ironman 1", "Ironman 2");
+mostrarPeliculas('+ ironman');
 
-peliculas.splice(2,0,'Thor');
-document.write(`<h2>Lista de peliculas + thor</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+peliculas.splice(2, 0, "Thor");
+mostrarPeliculas('+ thor')
 
-peliculas.push('Batman');
-document.write(`<h2>Lista de peliculas + Batman</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+peliculas.push("Batman");
+mostrarPeliculas('+ Batman')
 
 //modificar elementos en el array
-peliculas[3] = 'Dr. Strange: multiverse of madness'
-console.log(peliculas)
-document.write(`<h2>Lista de peliculas + Dr. strange modificado (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+peliculas[3] = "Dr. Strange: multiverse of madness";
+console.log(peliculas);
+mostrarPeliculas(`+ Dr. strange modificado (${peliculas.length})`)
 
 //borrar elementos del array
 peliculas.shift();
-document.write(`<h2>Lista de peliculas menos el primer elemento (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas(`menos el primer elemento (${peliculas.length})`)
 
 // peliculas.splice(6,1);
-peliculas.splice(6,2);
+peliculas.splice(6, 2);
 // peliculas.splice(6);
-document.write(`<h2>Lista de peliculas menos un elemento particular (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas(`menos un elemento particular (${peliculas.length})`)
 
 peliculas.pop();
-document.write(`<h2>Lista de peliculas menos el ultimo elemento (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas(`menos el ultimo elemento (${peliculas.length})`)
+
